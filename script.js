e8122867a002137f0d62cdef94bc25b8a917b7cd
Vinicodeze <viniciuspaulino300@gmail.com>
2025-11-02 23:50:30 -0300

    script adicionado

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-const menuToggle = document.querySelector('.menu-toggle');
-const mainNav = document.querySelector('.main-nav');
+const menuToggle = document.querySelector(".menu-toggle");
+const mainNav = document.querySelector(".main-nav");
 
-if (menuToggle && mainNav) {
-    menuToggle.addEventListener('click', () => {
-        mainNav.style.display = mainNav.style.display === 'block' ? 'none' : 'block';
+if(menuToggle && mainNav){
+    menuToggle.addEventListener("click", () => {
+        mainNav.classList.toggle("active");
     });
 }
